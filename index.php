517f2d2a8f079a8628e3dfcb834ce1d58c3ec823
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-06-05 17:20:55 +0200

    add scaffoding

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My little Shop</title>
+</head>
+<body>
+    
+    <header>
+        <h1>MY SHOP</h1>
+    </header>
+
+    <main>
+        <section class="products">
+            CONTENT HERE
+        </section>
+    </main>
+
+</body>
+</html>
